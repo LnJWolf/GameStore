57010dfaa633ed75f21d59eeaa0aa0060b755f50
Ivan Markov <outcastth@gmail.com>
2016-12-25 14:18:58 +0300

    Adding additional HtmlHelper class with unit-tests to it.

--- GameStore.UnitTests/UnitTest1.cs
+++ GameStore.UnitTests/UnitTest1.cs
@@ -7,6 +7,8 @@ using Moq;
 using GameStore.Domain.Abstract;
 using GameStore.Domain.Entities;
 using GameStore.WebUI.Controllers;
+using GameStore.WebUI.Models;
+using GameStore.WebUI.HtmlHelpers;
 
 namespace GameStore.UnitTests
 {
@@ -37,6 +39,35 @@ namespace GameStore.UnitTests
             Assert.IsTrue(games.Count == 2);
             Assert.AreEqual(games[0].Name, "Игра4");
             Assert.AreEqual(games[1].Name, "Игра5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+
+            // Организация - определение вспомогательного метода HTML - это необходимо
+            // для применения расширяющего метода
+            HtmlHelper myHelper = null;
+
+            // Организация - создание объекта PagingInfo
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // Организация - настройка делегата с помощью лямбда-выражения
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            // Действие
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            // Утверждение
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
+                + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
+                result.ToString());
         }
     }
 }
