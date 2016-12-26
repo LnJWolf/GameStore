be9e223be756ffd5800048e6fc0bec2c8bca1016
Ivan Markov <outcastth@gmail.com>
2016-12-27 01:30:08 +0300

    View Paging completed. Tommorow is time to add some style to it.

--- GameStore.UnitTests/UnitTest1.cs
+++ GameStore.UnitTests/UnitTest1.cs
@@ -21,21 +21,21 @@ namespace GameStore.UnitTests
             // Организация (arrange)
             Mock<IGameRepository> mock = new Mock<IGameRepository>();
             mock.Setup(m => m.Games).Returns(new List<Game>
-            {
-                new Game { GameId = 1, Name = "Игра1"},
-                new Game { GameId = 2, Name = "Игра2"},
-                new Game { GameId = 3, Name = "Игра3"},
-                new Game { GameId = 4, Name = "Игра4"},
-                new Game { GameId = 5, Name = "Игра5"}
-            });
+                {
+                    new Game { GameId = 1, Name = "Игра1"},
+                    new Game { GameId = 2, Name = "Игра2"},
+                    new Game { GameId = 3, Name = "Игра3"},
+                    new Game { GameId = 4, Name = "Игра4"},
+                    new Game { GameId = 5, Name = "Игра5"}
+                });
             GameController controller = new GameController(mock.Object);
             controller.pageSize = 3;
 
             // Действие (act)
-            IEnumerable<Game> result = (IEnumerable<Game>)controller.List(2).Model;
+            GamesListViewModel result = (GamesListViewModel)controller.List(2).Model;
 
-            // Утверждение (assert)
-            List<Game> games = result.ToList();
+            // Утверждение
+            List<Game> games = result.Games.ToList();
             Assert.IsTrue(games.Count == 2);
             Assert.AreEqual(games[0].Name, "Игра4");
             Assert.AreEqual(games[1].Name, "Игра5");
@@ -68,6 +68,34 @@ namespace GameStore.UnitTests
                 + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
                 + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
                 result.ToString());
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Организация (arrange)
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+                {
+                    new Game { GameId = 1, Name = "Игра1"},
+                    new Game { GameId = 2, Name = "Игра2"},
+                    new Game { GameId = 3, Name = "Игра3"},
+                    new Game { GameId = 4, Name = "Игра4"},
+                    new Game { GameId = 5, Name = "Игра5"}
+                });
+            GameController controller = new GameController(mock.Object);
+            controller.pageSize = 3;
+
+            // Act
+            GamesListViewModel result
+                = (GamesListViewModel)controller.List(2).Model;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
